d35d8bd5fd4030efbbd936f690d90f9229dc8b06
bluefox182 <fergaldoss@gmail.com>
2021-05-28 23:19:50 -0500

    adding more css sytle

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,8 @@ import "./Card.css";
 const Card = () => {
   return (
     <div className="card">
-      <img src="#" alt="lang.svg" />
-      <h3>Language</h3>
+      <img src="https://cdn.svgporn.com/logos/python.svg" alt="lang.svg" />
+      <h3>Python</h3>
     </div>
   );
 };
