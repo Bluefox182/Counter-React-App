850835a6d215a9ebef4df2d752dac9c58828083c
bluefox182 <fergaldoss@gmail.com>
2021-05-28 23:25:51 -0500

    using react fragment

---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+const Cards = () => {
+  return (
+    <React.Fragment>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </React.Fragment>
+  );
+};
+
+export default Cards;
